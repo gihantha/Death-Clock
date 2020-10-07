c1e092b1611992fd1cb4e4835cc75016a2ebe5a1
gihantha <gihantha01@gmail.com>
2020-10-07 20:14:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secondsEl = document.getElementById('seconds')
+
+const deathDay = "1 Jan 2026";
+ 
+function countdown() {
+    const deathDayDate = new Date(deathDay);
+    const currentDate = new Date();
+
+    const totalSeconds = (deathDayDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24) ;
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+   
+    daysEl.innerHTML = days
+}
+
+//initial call
+
+countdown();
+
+setInterval(countdown, 1000);
